36e631a93393bef35430e8e37cbaf170c8770fdf
You-Hao Chang <question0802@gmail.com>
2018-02-05 23:47:45 +0800

    Add files via upload

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -110,6 +110,11 @@ class LearningAgent(Agent):
         # When learning, choose a random action with 'epsilon' probability
         # Otherwise, choose an action with the highest Q-value for the current state
         # Be sure that when choosing an action with highest Q-value that you randomly select between actions that "tie".
+        if self.learning:
+            pass
+        else:
+            action = random.choice(self.env.valid_actions)
+
         return action
 
 
